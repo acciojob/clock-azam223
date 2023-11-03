797d062b5ba40952a039ae4c533118c0b70e7d8e
azam223 <135818819+azam223@users.noreply.github.com>
2023-11-03 17:27:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+function updateTimer() {
+  const timerElement = document.getElementById('timer');
+  const now = new Date();
+  const formattedDate = now.toLocaleDateString();
+  const formattedTime = now.toLocaleTimeString();
+
+  timerElement.textContent = `Current Date and Time: ${formattedDate} ${formattedTime}`;
+}
+
+setInterval(updateTimer, 1000); // Update every second
+updateTimer(); // Initial call
+
+window.onload = function() {
+  // Update the timer when the page loads
+  updateTimer();
+};
